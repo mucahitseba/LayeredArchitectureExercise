7c065e8188846801bfbdef871cf95e31fa91b365
mucahit.seba <mucahit.seba@inveon.com.tr>
2021-03-12 16:22:46 +0300

    entity framework implementation added

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace DataAccess.Abstract
+{
+    //IEntity : IEntity olabilir veya implemente eden bir nesne olabilir
+    //new() : new'lenebilir olmalı. IEntity newlenemeyeceği için onu devre dışı bıraktım.
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
