d04d7917a6c54705fb2bf8acaba7f51a8cb33c7b
mucahit.seba <mucahit.seba@inveon.com.tr>
2021-03-18 15:34:34 +0300

    business rule engine added

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -9,5 +9,8 @@ namespace Business.Constants
         public static string ProductAdded = "Ürün eklendi";
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
         public static string ProductsListed = "Ürünler listelendi";
+        public static string ProductCountOfCategoryError = "Belirtilen sayı aşıldı";
+        public static string ProductNameAlreadyExists = "Bu isimde ürün kaydı var";
+        public static string CategoryLimitExceded = "Kategori limiti aşıldı";
     }
 }
